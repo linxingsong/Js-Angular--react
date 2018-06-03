796fdc34064e04d9261332edb4d74c46cd69ce8b
XingSong Lin <linxingsong@gmail.com>
2018-06-02 23:51:06 -0400

    some

--- React/recipe/src/RecipeApp.js
+++ React/recipe/src/RecipeApp.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react';
 import Navbar from './navbar';
-import Recipe from './recipe';
 import RecipeList from './recipeList';
+import RecipeInput from './recipeInput';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      recipe: [
+        {
+          id: 0,
+          title: "Spaghetti",
+          instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "noodle.jpg"
+        },
+        {
+          id: 1,
+          title: "Milkshake",
+          instructions: "Combine ice cream and milk.  Blend until creamy",
+          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+          img: "milkshake.jpg"
+        },
+        {
+          id: 2,
+          title: "Avocado Toast",
+          instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
+          img: "avocado.jpg"
+        }
+      ], 
+      nextRecipeId: 3,
+    }
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  handleSave(recipe){
+    this.setState((prevState, props) => {
+      const newRecipe = {...recipe, id: this.state.nextRecipeId};
+      return {
+        nextRecipeId: prevState.nextRecipeId + 1,
+        recipes: [...this.state.recipes, newRecipe]
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeList />
+        <RecipeInput onSave={this.handleSave}/>
+        <RecipeList recipes={this.state.recipe} />
       </div>
     );
   }
